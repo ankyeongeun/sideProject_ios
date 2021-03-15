4457659a2765e836ace35d774ae495dbd370ad19
ankyeongeun <anruddmds8124@gmail.com>
2021-03-15 18:11:29 +0900

    expo setting & connect to server

---
+++ App.tsx
@@ -0,0 +1,57 @@
+import React from 'react';
+import { StyleSheet, Text, View, ActivityIndicator } from 'react-native';
+
+interface IMyComponentProps {
+}
+
+interface IMyComponentState {
+  isLoading: boolean,
+  dataSource?: string
+}
+
+export default class App extends React.Component<IMyComponentProps, IMyComponentState> {
+  
+  constructor(props: any) {
+    super(props);
+    this.state = { isLoading: true};
+  }
+
+  componentDidMount(){
+    return fetch('http://localhost:3000')
+      .then((response) => response.text())
+      .then((responseString) => {
+        this.setState({
+          isLoading: false,
+          dataSource: responseString
+        });
+      })
+      .catch((error) =>{
+        console.error(error);
+      });
+  }
+
+  render(){
+    if(this.state.isLoading){
+      return(
+        <View style={{flex: 1, padding: 20}}>
+          <ActivityIndicator/>
+        </View>
+      )
+    }
+
+    return (
+      <View style={styles.container}>
+        <Text>{this.state.dataSource}</Text>
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
